aa49603cc8b35d7cde1a1fe898366ea2d673c10d
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-12-05 09:02:10 +0530

    javascript added

---
+++ calculator.js
@@ -0,0 +1,40 @@
+function fun_add() {
+    let a = parseInt(document.getElementById("a1").value)
+    let b = parseInt(document.getElementById("a2").value)
+    let h3 = document.getElementById("h3")
+    h3.innerHTML=a+b
+
+    
+}
+function fun_sub() {
+    let a = parseInt(document.getElementById("a1").value)
+    let b = parseInt(document.getElementById("a2").value)
+    let h3 = document.getElementById("h3")
+    h3.innerHTML = a - b
+
+
+}
+function fun_mult() {
+    let a = parseInt(document.getElementById("a1").value)
+    let b = parseInt(document.getElementById("a2").value)
+    let h3 = document.getElementById("h3")
+    h3.innerHTML = a * b
+
+
+}
+function fun_div() {
+    let a = parseInt(document.getElementById("a1").value)
+    let b = parseInt(document.getElementById("a2").value)
+    let h3 = document.getElementById("h3")
+    h3.innerHTML = a / b
+
+
+}
+function fun_mod() {
+    let a = parseInt(document.getElementById("a1").value)
+    let b = parseInt(document.getElementById("a2").value)
+    let h3 = document.getElementById("h3")
+    h3.innerHTML = a % b
+
+
+}
---
+++ sample.js
@@ -0,0 +1,13 @@
+// function fun1() {
+//     let a = parseInt(document.getElementById("demo").value)
+//     console.log(typeof a);
+
+//     console.log('welcome');
+
+// }
+
+function fun1() {
+    let a= document.getElementById("demo").value
+    let h3 = document.getElementById("h3")
+    h3.innerHTML=a
+}
